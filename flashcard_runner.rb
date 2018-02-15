c78816861a25dc4d3bfb050e959650cc417f39b1
JerrelMitchell <jerrelmitchell@ymail.com>
2018-02-14 21:56:14 -0700

    continue methods and creation of runner for flash cards game

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -3,18 +3,27 @@ require './lib/flash_cards'
 require './lib/guess'
 require './lib/round'
 
+require 'pry'
+
 class GameRunner
+  def initialize
+    @card1 = Card.new('What is the capital of Alaska?', 'Juneau')
+    @card2 = Card.new('Approximately how many miles are in
+                      one astronomical unit?', '93,000,000')
+    @card3 = Card.new('Describe in words the exact direction that is 697.5°
+                      clockwise from due north?', 'North north west')
+    @deck  = Deck.new([card1, card2, card3])
+    @round = Round.new(deck)
+  end
 
   def start
     "Type 'Start' to begin a new round!"
   end
 
   def header_welcome
-    "Welcome! You're playing with #{deck.count} cards."
-  end
-
-  def header_end
-    '___________________________________________'
+    header_message = "Welcome! You're playing with #{deck.count} cards.\n
+                      _________________________________________________"
+    header_message
   end
 
   def footer
@@ -26,16 +35,20 @@ class GameRunner
   end
 
   def score
-    "You had #{number_correct} correct guesses out of #{deck.count} for a score of #{percent_correct}%."
+    score_message = "You had #{round.number_correct} correct guesses out of
+                     #{deck.count} for a score of #{round.percent_correct}%."
+    score_message
   end
 
   def show_current_card
-    "This is card number #{round.} out of #{round.guesses.count}."
+    "This is card number #{round.guesses.count} out of #{deck.count}."
   end
 end
 
-puts header_welcome
-puts header_end
+game = GameRunner.new
+
+puts game.header_welcome
+puts game.header_end
 puts start
 gets.chomp.capitalize
 puts show_current_card
@@ -46,20 +59,11 @@ puts feedback
 puts footer
 puts score
 
-
-# card1 = Card.new('What is the capital of Alaska?', 'Juneau')
-# card2 = Card.new('Approximately how many miles are in
-#                   one astronomical unit?', '93,000,000')
-# card3 = Card.new('Describe in words the exact direction that is 697.5°
-#                   clockwise from due north?', 'North north west')
-# deck  = Deck.new([card1, card2, card3])
-# round = Round.new(deck)
-#
-# # assert_equal 'Juneau', round.record_guess('Juneau')
-# # assert_equal 1, round.guesses.count
-# # assert_equal 'Correct!', round.guesses.first.feedback
-# # assert_equal '2', round.record_guess('2')
-# # assert_equal 2, round.guesses.count
-# # assert_equal 'Incorrect.', round.guesses.last.feedback
-# # assert_equal 1, round.number_correct
-# # assert_equal 50, round.percent_correct
+round.record_guess(gets.chomp)
+round.guesses.count
+puts round.guesses.first.feedback
+round.record_guess(gets.chomp)
+round.guesses.count
+puts round.guesses.last.feedback
+round.number_correct
+round.percent_correct
